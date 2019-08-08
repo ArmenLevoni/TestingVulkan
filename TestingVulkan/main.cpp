3c4c2a2d2b0e565943e05b4777fe460040f5959e
Armen <armkha01@arm.com>
2019-08-08 16:23:09 +0100

    Initial Commit

---
+++ TestingVulkan/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TestingVulkan
+//
+//  Created by Armen Khachatryan on 08/08/2019.
+//  Copyright © 2019 Armen Khachatryan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
